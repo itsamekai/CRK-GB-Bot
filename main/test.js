7f770d055f086122af49964e0d35e6168040a423
itsamekai <resayz4@ymail.com>
2022-07-24 17:54:47 +0800

    added contribution scrolling ss with cropping

--- main/test.js
+++ main/test.js
@@ -1,7 +1,10 @@
 const { createWorker } = require('tesseract.js');
 const path = require('path')
+const { getPlayerAndCount } = require('../lib/screenshot')
+const robot = require('robotjs')
+const ks = require('node-key-sender')
 
-// const worker = createWorker();
+const worker = createWorker();
 // // const rectangle = { left: 914, top: 915, width: 320, height: 50 };
 // const rectangle = { left: 924, top: 922, width: 309, height: 31 };
 // // left is the distance from the upper-left corner of the bounding box, to the left border of the image.
@@ -13,23 +16,35 @@ const path = require('path')
 //   await worker.load();
 //   await worker.loadLanguage('eng');
 //   await worker.initialize('eng');
-//   await worker.setParameters({
-//   tessedit_char_whitelist: '0123456789()%',
-//   // tessedit_char_whitelist: '0123456789Lv.',
-//     });
+//   // await worker.setParameters({
+//   // tessedit_char_uar_whitelist: '0123456789Lv.',
+//   //   });
 
-//   const a = path.join(__dirname, "../images/health.png")
+//   const a = path.join(__dirname, "../images/test4.png")
 //   console.log(a)
 
-//   const { data: { text } } = await worker.recognize(a, { rectangle });
+//   const { data: { text } } = await worker.recognize(a);
 //   console.log(text)
 //   await worker.terminate();
-    
+//   const result = text.split(/\r?\n/)
+//   console.log(result[0])       
 // })();
 
-let text = 22235351212
-console.log(addCommas(text))
+(async () => {
+  await getPlayerAndCount()
+
+}
+
+)()
+// let text = 22235351212
+// console.log(addCommas(text))
+
+
 
 function addCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function sleep(millis) {
+  return new Promise(resolve => setTimeout(resolve, millis));
 }
